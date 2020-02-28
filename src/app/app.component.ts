f747a3ecb36e210a04b17aae4bac9b3104ca5d7f
Sonu Kumar <sonu.kumar@zs.com>
2020-02-28 13:34:40 +0530

    content-projectiom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,44 +8,13 @@ import { CourseCardComponent } from './course-card/course-card.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-
-  ngAfterViewInit(): void {
-  //  console.log('Card 1',this.card1); 
-  console.log(this.cards);
-
-  this.cards.changes.subscribe(cards => console.log(cards));
-  
-  }
-
-  
+export class AppComponent  {
+ 
   courses: Array<Course> = COURSES;
-  //  coreCourse:Course = COURSES[0];
-  //  RxjsCourse:Course = COURSES[1];
-  //  NgrxCourse:Course = COURSES[2];
-
-  title = COURSES[0].description;
-  startDate = new Date(2000, 0, 1);
-
-  course = COURSES[0];
-
-  @ViewChild('cardRef1', {read: ElementRef})
-  card1: ElementRef;
-
-
-  @ViewChild('cardRef2')
-  card2: CourseCardComponent;
-
-  @ViewChildren(CourseCardComponent, {read:ElementRef})
-  cards:QueryList<ElementRef>;
 
   oncourseClicked(course: Course) {
-    // console.log(this.card1);
-    // console.log(this.card2);
-
+    console.log(course);
+    
   }
 
-  editCourses(){
-    this.courses.pop();
-  }
 }
